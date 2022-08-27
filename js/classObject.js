f4c1ba5521b5c17dc2b586775a0b7e1fd0e36a06
Pereyra Jose <luispereyra14@hotmail.com>
2022-08-27 12:43:53 -0300

    prototype de pelicula creado

---
+++ js/classObject.js
@@ -0,0 +1,9 @@
+export default class prototypePelis{
+    constructor(codigo,titulo,descripcion,imagen, genero){
+        this.codigo=codigo;
+        this.titulo=titulo;
+        this.descripcion=descripcion;
+        this.imagen=imagen;
+        this.genero=genero; 
+    }
+}
