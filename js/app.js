9b6284d327beb89216feaa7ae0adbae47650acb2
Pereyra Jose <luispereyra14@hotmail.com>
2022-08-27 20:03:11 -0300

    creamos el objeto de peliculas, agregamos al array y guardamos en local storage, falta limpiar form

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
+import prototypePelis from "./classObject.js";
 import{cantidadCaracteres,validDescription,validImagen,validGenero} from "./helper.js"
 
 
 
 
 // Iniciamos Variables
-
+let listaPeliculas = JSON.parse(localStorage.getItem("localStoragePeli"))||[];
+const modalFormPeliculas = new bootstrap.Modal(document.querySelector("#modalForm"));
+const btnCrearPelicula = document.getElementById("btnCrearPelicula")
 let codigo = document.getElementById("codigo");
 let titulo = document.getElementById("titulo");
 let descripcion = document.getElementById("descripcion");
@@ -13,6 +16,7 @@ let genero = document.getElementById("genero");
 let formPelicula = document.getElementById("formPelicula");
 
 // events
+btnCrearPelicula.addEventListener("click",()=>{mostrarForm(codigo)})
 formPelicula.addEventListener("submit",crearPelicula);
 codigo.addEventListener("blur",()=>{cantidadCaracteres(codigo)});
 titulo.addEventListener("blur",()=>{cantidadCaracteres(titulo)});
@@ -20,7 +24,25 @@ descripcion.addEventListener("blur",()=>{validDescription(descripcion)});
 imagen.addEventListener("blur",()=>{validImagen(imagen)});
 genero.addEventListener("blur",()=>{validGenero(genero)});
 
+
+
+function mostrarForm(){
+  modalFormPeliculas.show();
+  codigo.value = uuidv4();
+}
+
+
+
 function crearPelicula(e){
   e.preventDefault();
-  console.log("vamo a crea la pelicula")
+  console.log("vamo a crea la pelicula");
+  const IngresarPelicula = new prototypePelis(codigo.value, titulo.value, descripcion.value, imagen.value, genero.value);
+  console.log(IngresarPelicula);
+  listaPeliculas.push(IngresarPelicula);
+  console.log(listaPeliculas);
+  guardarDatosSL();
+}
+
+function guardarDatosSL(){
+  localStorage.setItem("localStoragePeli", JSON.stringify(listaPeliculas))
 }
